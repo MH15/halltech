1da459c947732909d1354866738e35ceac8788dc
PeterHall16 <peterhall330@outlook.com>
2017-02-18 15:24:33 -0500

    Form stuff
    The form is now working using PHPMailer

--- contact.php
+++ contact.php
@@ -1,34 +1,30 @@
 <?php
+    $name = strip_tags($_POST["name"]);
+    $email = strip_tags($_POST["email"]);
+    $phone = strip_tags($_POST["phone"]);
+    $budget = strip_tags($_POST["budget"]);
+    $message = strip_tags($_POST["message"]);
 
-$api_key = "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";
-$template_id = "c22c546b-968a-44a3-b5e4-4016cd30b49c";
+    $text = "<b>Fullname: </b>" . $name . "<br>" .
+    "<b>E-Mail: </b> " . $email . "<br>" .
+    "<b>Phone: </b> " . $phone . "<br>" .
+    "<b>Budget: </b> " . $budget . "<br>" .
+    "<b>Message: </b> " . $message .
 
-$fullname = strip_tags($_POST["fullname"]);
-$email = strip_tags($_POST["email"]);
-$phone = strip_tags($_POST["phone"]);
-$content = strip_tags($_POST["message"]);
+    require 'assets/PHPMailerAutoload.php';
+    
+    $mail = new PHPMailer;
+    $mail->setFrom('Contact Form');
+    $mail->addAddress('info@halltechnologies.co', 'Native Tech');
+    $mail->Subject  = 'Form Submission';
+    
+    $mail->isHTML(true);
+    $mail->Body = $text;
 
-$text = "<b>Fullname: </b>" . $fullname . "<br>" .
-"<b>E-Mail: </b> " . $email . "<br>" .
-"<b>Phone: </b> " . $phone . "<br>" .
-"<b>Message: </b> " . nl2br($content);
-
-require 'vendor/autoload.php';
-
-$from = new SendGrid\Email("The Luminous Tech", "theluminoustech@gmail.com");
-$subject = $fullname;
-$to = new SendGrid\Email("James Barr", "jamesbarrbusiness@gmail.com");
-$content = new SendGrid\Content("text/html", $text);
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-$mail->setTemplateId($template_id);
-$mail->setReplyTo(new SendGrid\Email($fullname, $email));
-
-$sg = new \SendGrid($api_key);
-
-$response = $sg->client->mail()->send()->post($mail);
-
-if($response->statusCode() == 200 || $response->statusCode() == 202) {
-	header("Location: thanks.html");
-} else {
-	header("Location: error.html");
-}
+    if(!$mail->send()) {
+        echo 'Message was not sent.';
+        echo 'Mailer error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message has been sent.';
+    }
+?>
