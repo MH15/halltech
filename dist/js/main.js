611e74a86aa275991ddb8b3dd46f55f38da5ea44
MH15 <matthew349hall@hotmail.com>
2017-02-20 21:08:22 -0500

    New stuff
    Almost done!
    Added Contact form, portfolio (not done)
    also did header

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,13 +14,40 @@ button.onclick = closeHeader
 
 function closeHeader () {
 	if ( parseInt(header.style.left) < 0) {
-		console.log("opening")
 		header.style.left = "0"
 		button.style.boxShadow = "none"
 
 	} else {
-		console.log("closing")
 		header.style.left = (-window.innerWidth).toString() + "px"
 		button.style.boxShadow = "0 0 8px 1px #181818b3"
 	}
+}
+
+
+
+
+var modal = picker("section#contact-form"),
+	contactButton = picker("#button"),
+	blackout = picker("#blackout")
+
+
+contactButton.onclick = openContactForm
+
+blackout.onclick = closeContactForm
+
+
+
+function openContactForm () {
+	modal.style.visibility = "visible"
+	modal.style.opacity = 1
+	blackout.style.visibility = "visible"
+	blackout.style.opacity = 0.5
+}
+
+function closeContactForm () {
+	console.log("closing")
+	modal.style.opacity = 0
+	modal.style.visibility = "hidden"
+	blackout.style.opacity = 0
+	blackout.style.visibility = "hidden"
 }
